e3906466daecb4a38e1202da8cb683d3eb3a50ac
JosephBurnitz <jburnitz@gmail.com>
2013-12-05 13:13:03 -0600

    added default lives to 3

--- editormainwindow.cpp
+++ editormainwindow.cpp
@@ -96,8 +96,8 @@ void editWindow::on_actionAutomatic_Snapping_changed()
 
 void editWindow::on_actionSave_triggered()
 {
+    ginny->life = 3;
     ginny->ClickedSaveMap();
-    //ginny->saveGame( QString("poop") );
 }
 
 void editWindow::on_radioButtonMBLOCK_clicked()
